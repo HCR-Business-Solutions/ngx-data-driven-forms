d8bbd52e3338c2e23b71be08a3469fc148f0c9f1
Rei Armenia <67706744+reiarmenia@users.noreply.github.com>
2022-03-03 10:46:40 -0500

    do options resolving #29

--- projects/ngx-data-driven-forms/src/lib/ddforms/services/index.ts
+++ projects/ngx-data-driven-forms/src/lib/ddforms/services/index.ts
@@ -3,3 +3,4 @@ export * from './data-driven-forms-config.service';
 export * from './data-driven-forms-events.service';
 export * from './data-driven-forms-validation.service';
 export * from './data-driven-forms.service';
+export * from './options-data.service';
---
+++ projects/ngx-data-driven-forms/src/lib/ddforms/services/options-data.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject, map, Observable, shareReplay} from 'rxjs';
+import {IQuestionOption} from '../../shared';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OptionsDataService {
+
+  private readonly optionsData: BehaviorSubject<Map<string, IQuestionOption[]> | null | undefined> = new BehaviorSubject<Map<string, IQuestionOption[]> | null | undefined>(null);
+  public readonly optionsData$: Observable<Map<string, IQuestionOption[]> | null | undefined> = this.optionsData.asObservable().pipe(
+    shareReplay(1),
+  );
+
+  constructor() { }
+
+  registerData(key: string, data: IQuestionOption[]): void {
+
+    const dataMap: Map<string, IQuestionOption[]> = this.optionsData.getValue() ?? new Map<string, IQuestionOption[]>();
+    dataMap.set(key, data);
+    this.optionsData.next(dataMap);
+
+  }
+
+  getKeyStream(key: string): Observable<IQuestionOption[] | null | undefined> {
+    return this.optionsData$.pipe(
+      map(dataMap => dataMap?.get(key) ?? null)
+    );
+  }
+
+}
