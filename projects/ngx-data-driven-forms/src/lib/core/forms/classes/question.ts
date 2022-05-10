2035d963fa473228cf2ec50956a7500504d79255
Rei Armenia <67706744+reiarmenia@users.noreply.github.com>
2022-05-10 14:32:05 -0400

    ➕Question Class; ✨Optimize Validator Gathering

--- projects/ngx-data-driven-forms/src/lib/core/forms/classes/question.ts
+++ projects/ngx-data-driven-forms/src/lib/core/forms/classes/question.ts
@@ -1,5 +1,67 @@
-import { IQuestion, IQuestionAccessible } from '../interfaces';
+import { FormControl, ValidatorFn } from '@angular/forms';
+import { FieldValidatorFn } from '../../types';
+import {
+  ICrossFieldValidationPack,
+  IQuestion,
+  IRendererConfig,
+  IShouldAsk,
+} from '../interfaces';
 
-type QuestionImplementation = IQuestion | IQuestionAccessible;
+export class Question implements IQuestion {
+  id: string;
+  type: string;
+  label?: string | undefined;
+  ariaLabel?: string | undefined;
+  placeholder?: string | undefined;
+  hint?: string | undefined;
+  readonly?: boolean | undefined;
+  isFlag?: boolean | undefined;
+  fieldValidation?: { [key: string]: any } | undefined;
+  crossFieldValidation?: ICrossFieldValidationPack[] | undefined;
+  shouldAsk?: IShouldAsk | undefined;
+  rendererConfig?: IRendererConfig | undefined;
+  customProps?: { [key: string]: any } | undefined;
 
-export class Question implements QuestionImplementation {}
+  constructor(question: IQuestion) {
+    this.id = question.id;
+    this.type = question.type;
+    this.label = question.label;
+    this.ariaLabel = question.ariaLabel;
+    this.placeholder = question.placeholder;
+    this.hint = question.hint;
+    this.readonly = question.readonly;
+    this.isFlag = question.isFlag;
+    this.fieldValidation = question.fieldValidation;
+    this.crossFieldValidation = question.crossFieldValidation;
+    this.shouldAsk = question.shouldAsk ? question.shouldAsk : undefined;
+    this.rendererConfig = question.rendererConfig;
+    this.customProps = question.customProps;
+  }
+
+  public control(
+    initialValue: any,
+    knownFieldValidators: Map<string, FieldValidatorFn>
+  ): FormControl {
+    return new FormControl(
+      initialValue ?? null,
+      this.getValidators(knownFieldValidators),
+      null
+    );
+  }
+
+  public getValidators(
+    knownFieldValidators: Map<string, FieldValidatorFn>
+  ): ValidatorFn[] {
+    if (!this.fieldValidation) return [];
+    return Object.entries(this.fieldValidation)
+      .map(([key, val]) => ({
+        val, // keep the value of the entry
+        fieldValidator: knownFieldValidators.get(key), //use the key of the entry to attempt to get a function
+      }))
+      .map(
+        ({ val, fieldValidator }) =>
+          fieldValidator ? fieldValidator(val) : undefined //try to run the function, go to undefined if not exist
+      )
+      .filter((validator): validator is ValidatorFn => !!validator); // remove all undefined and fix type.
+  }
+}
