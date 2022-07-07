e1416b105f74c953aea376634321038172a7b117
Rei Armenia <67706744+reiarmenia@users.noreply.github.com>
2022-07-07 14:43:39 -0400

    begin tech mock up

--- projects/info-ngx-ddforms-web/src/app/app.component.ts
+++ projects/info-ngx-ddforms-web/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {
+  Application,
+  FormGenerationService,
+} from 'ngx-data-driven-forms/src/public-api';
 
 @Component({
   selector: 'app-root',
   template: `<div>
     <app-toolbar></app-toolbar>
-    <main>
+    <main class="bg-white">
       <router-outlet></router-outlet>
     </main>
   </div>`,
