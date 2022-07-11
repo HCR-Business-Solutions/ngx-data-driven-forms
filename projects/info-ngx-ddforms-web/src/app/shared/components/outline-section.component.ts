05fd77ae0aae17cd7340f48ac1c300d68e9e0744
Rei Armenia <67706744+reiarmenia@users.noreply.github.com>
2022-07-11 14:18:18 -0400

    make mobile section title be inside outline

--- projects/info-ngx-ddforms-web/src/app/shared/components/outline-section.component.ts
+++ projects/info-ngx-ddforms-web/src/app/shared/components/outline-section.component.ts
@@ -6,9 +6,7 @@ import { Component, OnInit } from '@angular/core';
     <section
       class="p-2 relative border-2 border-inherit rounded-tl-2xl rounded-br-2xl rounded-tr-xl rounded-bl-xl"
     >
-      <div
-        class="absolute top-[-40px] sm:top-[-15px] left-[8px] sm:left-10 bg-white px-4"
-      >
+      <div class="relative sm:absolute sm:top-[-15px] sm:left-10 bg-white px-4">
         <ng-content select="[sectionTitle]"></ng-content>
       </div>
       <ng-content select="[sectionContent]"></ng-content>
