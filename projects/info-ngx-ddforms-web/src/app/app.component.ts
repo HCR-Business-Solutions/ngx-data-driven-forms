b7e8358f901524b50eaa2a50973a5ce637d8ce74
Rei Armenia <67706744+reiarmenia@users.noreply.github.com>
2022-09-08 12:38:35 -0400

    return showcase to appropriate state

--- projects/info-ngx-ddforms-web/src/app/app.component.ts
+++ projects/info-ngx-ddforms-web/src/app/app.component.ts
@@ -1,88 +1,15 @@
 import { Component } from '@angular/core';
-import { FormGenerationService } from 'dist/ngx-data-driven-forms/public-api';
-import { Application } from 'ngx-data-driven-forms/src/lib/core';
 
 @Component({
   selector: 'app-root',
   template: `<div>
-    <ddforms-application-container
-      [application]="this.applicationObject"
-    ></ddforms-application-container>
+    <app-toolbar></app-toolbar>
+    <main class="bg-white">
+      <router-outlet></router-outlet>
+    </main>
   </div>`,
   styles: [],
 })
 export class AppComponent {
-  constructor(public formGenerator: FormGenerationService) {}
-
-  applicationObject = new Application({
-    id: 'exampleApp',
-    description: 'an example application',
-    pages: [
-      {
-        id: 'contact',
-        sections: [
-          {
-            id: 'name',
-            questions: {
-              firstName: {
-                id: 'firstName',
-                type: 'text',
-                label: 'First Name',
-                fieldValidation: {
-                  required: true,
-                },
-              },
-              lastName: {
-                id: 'lastName',
-                type: 'text',
-                label: 'Last Name',
-                fieldValidation: {
-                  required: true,
-                },
-              },
-            },
-            layout: ['firstName', 'lastName'],
-          },
-          {
-            id: 'address',
-            questions: {
-              address: {
-                id: 'address',
-                type: 'text',
-                label: 'Address',
-                fieldValidation: {
-                  required: true,
-                },
-              },
-              city: {
-                id: 'city',
-                type: 'text',
-                label: 'City',
-                fieldValidation: {
-                  required: true,
-                },
-              },
-              state: {
-                id: 'state',
-                type: 'text',
-                label: 'State',
-                fieldValidation: {
-                  required: true,
-                },
-              },
-              zip: {
-                id: 'zip',
-                type: 'text',
-                label: 'Zip',
-                fieldValidation: {
-                  required: true,
-                },
-              },
-            },
-            layout: ['address', 'city', 'state', 'zip'],
-          },
-        ],
-      },
-    ],
-  });
+  constructor() {}
 }
